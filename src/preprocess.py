c7f4579671c9c28a3c5a10ecda94ced53d35f7bb
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:38:08 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,9 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
-
 def preprocess_data(df: pd.DataFrame):
-    # Drop unnecessary columns
     df = df.drop(
         columns=[
             "UDI",
@@ -15,12 +13,10 @@ def preprocess_data(df: pd.DataFrame):
             "RNF",
         ]
     )
-
-    # Encode Machine Type
+    
     encoder = LabelEncoder()
     df["Type"] = encoder.fit_transform(df["Type"])
 
-    # Features and Target
     X = df.drop("Machine failure", axis=1)
     y = df["Machine failure"]
 
