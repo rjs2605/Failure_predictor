deaac3e16b6b1ca9f9d3cb7ec1605f004f911818
rjs2605 <127687656+rjs2605@users.noreply.github.com>
2026-07-06 10:39:00 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -10,36 +10,25 @@ from src.preprocess import preprocess_data
 
 
 def main():
-
     df = pd.read_csv("data/ai4i2020.csv")
-
     X, y, encoder = preprocess_data(df)
-
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
         test_size=0.2,
         random_state=42,
     )
-
     model = RandomForestClassifier(
         n_estimators=200,
         random_state=42,
     )
-
     model.fit(X_train, y_train)
-
     predictions = model.predict(X_test)
-
     print(f"Accuracy : {accuracy_score(y_test, predictions):.4f}")
-
     os.makedirs("models", exist_ok=True)
-
     joblib.dump(model, "models/model.pkl")
     joblib.dump(encoder, "models/encoder.pkl")
-
     print("Model Saved Successfully")
-
 
 if __name__ == "__main__":
     main()
